cee6b249d7ff7699c061c5d89cc27d70c278c83c
mmheller <mm_heller@yahoo.com>
2018-06-14 18:36:26 -0600

    adding layers and popup

--- extras/MH_Setup.js
+++ extras/MH_Setup.js
@@ -43,17 +43,26 @@ define([
     "dijit/registry",
     "dojo/mouse",
     "dojo/on",
-    "esri/map"
+    "esri/map",
+        "esri/dijit/InfoWindowLite",
+        "esri/InfoTemplate",
+        "esri/layers/FeatureLayer",
+        "dojo/dom-construct",
+        "dojo/domReady!"
 ], function (
             MH_Zoom2FeatureLayers, webMercatorUtils, declare, lang, esriRequest, all, urlUtils, FeatureLayer, Query, All,
             Scalebar, sniff, scaleUtils, request, arrayUtils, Graphic, Editorall, SnappingManager, FeatureLayer,
         SimpleRenderer, PictureMarkerSymbol, SimpleFillSymbol, SimpleLineSymbol,
-        CheckBox, Toolbar, Color, LabelLayer, TextSymbol, Polygon, InfoTemplate, dom, domClass, registry, mouse, on, Map
+        CheckBox, Toolbar, Color, LabelLayer, TextSymbol, Polygon, InfoTemplate, dom, domClass, registry, mouse, on, Map,
+          InfoWindowLite,
+          InfoTemplate,
+          FeatureLayer,
+          domConstruct
 ) {
 
     return declare([], {
         Phase1: function () {
-            var H2O_ID = getTokens()['CEDID'];    //*****************************************************Justin Change this to your Django CEID id variable!!!!!!!!
+            var H2O_ID = getTokens()['CEDID'];   
             if (typeof H2O_ID != 'undefined') {
                 var arrayCenterZoom = [-111, 45.5];
                 var izoomVal = 5;
@@ -61,7 +70,7 @@ define([
                 var arrayCenterZoom = [-111, 45.5];
                 var izoomVal = 10;
             }
-
+                        
             esri.config.defaults.geometryService = new esri.tasks.GeometryService("https://utility.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
             app.map = new esri.Map("map", { basemap: "topo", center: arrayCenterZoom, zoom: izoomVal, slider: true, sliderPosition: "bottom-right" });
             if (app.map.loaded) {
@@ -69,6 +78,12 @@ define([
             } else {
                 app.map.on("load", function () { mapLoaded(); });
             }
+
+            var infoWindow = new InfoWindowLite(null, domConstruct.create("div", null, null, app.map.root));
+            infoWindow.startup();
+            app.map.setInfoWindow(infoWindow);
+
+
             var scalebar = new Scalebar({ map: app.map, scalebarUnit: "dual" });
             app.loading = dojo.byId("loadingImg");  //loading image. id
             dojo.connect(app.map, "onUpdateStart", showLoading);
@@ -79,21 +94,45 @@ define([
                 domConstruct.destroy(component);                //destroy it
             }
 
-
             var portalUrl4Shapefile = "https://www.arcgis.com";
             var strHFL_URL = "https://services.arcgis.com/QVENGdaPbd4LUkLV/arcgis/rest/services/UMHW/FeatureServer/";
-            pUMHWFeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "3", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, "opacity": 0.6, outFields: ['*']});
 
-            pUMHW_MASKFeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "4", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, "opacity": 0.6, outFields: ['*'] });
+            var template = new InfoTemplate();
+            template.setTitle("<b>${GageTitle}</b>");
+            template.setContent("Watershed:${Watershed}<br><a href=${GageURL} target='_blank'>Link to gage at ${Agency} website</a>");
+            pGageFeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "0", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, infoTemplate: template, outFields: ['*'] });
+
+            pUMHWFeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "4", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, "opacity": 0.5, outFields: ['*'] });
+            pUMHW_MASKFeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "5", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, "opacity": 0.6, outFields: ['*'] });
+
+            var templateFAS = new InfoTemplate();
+            templateFAS.setTitle("<b>${NAME} MT FAS (Fishing Access Site)</b>");
+            templateFAS.setContent("${BOAT_FAC}<br><a href=${WEB_PAGE} target='_blank'>Link to Fish Access Site</a>");
+            pFASFeatureLayer = new esri.layers.FeatureLayer("https://services3.arcgis.com/Cdxz8r11hT0MGzg1/arcgis/rest/services/FWPLND_FAS_POINTS/FeatureServer/0",
+                                                        { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, infoTemplate: templateFAS, "opacity": 0.3, outFields: ['*'] });
+            
+
+            var templateBLM = new InfoTemplate();
+            templateBLM.setTitle("<b>${Facility_Name} BLM Facility</b>");
+            templateBLM.setContent("<a href=${URL} target='_blank'>Link to BLM Facility</a>");
+            pBLMFeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "1",
+                                                        { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, infoTemplate: templateBLM, "opacity": 0.6, outFields: ['*'] });
+
+
+            var templateFWP = new InfoTemplate();
+            templateFWP.setTitle("<b>${TITLE}</b>");
+            templateFWP.setContent("${WATERBODY}<br>${DESCRIPTION} Publish Date: ${PUBLISHDATE}");
+            pFWPFeatureLayer = new esri.layers.FeatureLayer("https://services3.arcgis.com/Cdxz8r11hT0MGzg1/arcgis/rest/services/WaterbodyRestrictions/FeatureServer/0",
+            { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, infoTemplate: templateFWP, "opacity": 0.6, outFields: ['*'] });
+
 
             var strlabelField1 = "Name";
-            pHUC8FeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "5", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, "opacity": 0.6, outFields: [strlabelField1] });
+            pHUC8FeatureLayer = new esri.layers.FeatureLayer(strHFL_URL + "6", { mode: esri.layers.FeatureLayer.MODE_ONDEMAND, "opacity": 0.6, outFields: [strlabelField1] });
             if (typeof H2O_ID != 'undefined') {
                 pHUC8FeatureLayer.setDefinitionExpression("(project_id = " + H2O_ID + ")");
             } else {
-                pHUC8FeatureLayer.setDefinitionExpression("HUC8 in (10020004,10020005,10020006,10020008,10020007,10020003,10020002,10020001)");
+                pHUC8FeatureLayer.setDefinitionExpression("HUC_8_12 in ('10020004','10020005','10020006','10020008_1','10020008_2','10020007','10020003','10020002','10020001','10030101_1')");
             }
-
 
             var vGreyColor = new Color("#666");              // create a text symbol to define the style of labels
             var pLabel1 = new TextSymbol().setColor(vGreyColor);
@@ -103,7 +142,8 @@ define([
             var plabels1 = new LabelLayer({ id: "labels1" });
             plabels1.addFeatureLayer(pHUC8FeatureLayer, pLabelRenderer1, "{" + strlabelField1 + "}");
 
-            app.map.addLayers([pUMHWFeatureLayer, pUMHW_MASKFeatureLayer, pHUC8FeatureLayer, plabels1]);
+            app.map.addLayers([pUMHWFeatureLayer, pUMHW_MASKFeatureLayer, pHUC8FeatureLayer, pFWPFeatureLayer, pBLMFeatureLayer, pFASFeatureLayer, pGageFeatureLayer, plabels1]);
+            app.map.infoWindow.resize(300, 65);
 
             app.pSup = new MH_Zoom2FeatureLayers({}); // instantiate the class
             app.dblExpandNum = 1;
@@ -112,7 +152,9 @@ define([
             } else {
                 app.pSup.qry_Zoom2FeatureLayerExtent(pUMHWFeatureLayer);
             }
-            
+
+
+
             function err(err) {
                 console.log("Failed to get stat results due to an error: ", err);
             }
